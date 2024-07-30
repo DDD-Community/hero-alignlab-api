92e2a03835146d0c9fab68798fab8c450faacadb
DongGeon0908 <wrjssmjdhappy@gmail.com>
2024-07-30 23:08:51 +0900

    fix: s3Client upload refac

--- src/main/kotlin/com/hero/alignlab/client/s3/client/S3Client.kt
+++ src/main/kotlin/com/hero/alignlab/client/s3/client/S3Client.kt
@@ -6,7 +6,10 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
 import com.hero.alignlab.client.s3.config.S3Properties
 import com.hero.alignlab.common.extension.encodeURL
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.withContext
+import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Component
@@ -18,34 +21,46 @@ class S3Client(
     private val amazonS3: AmazonS3
 ) {
     suspend fun upload(filePart: FilePart): String {
-        return filePart.content()
-            .reduce { buffer1, buffer2 -> buffer1.write(buffer2) }
-            .flatMap { dataBuffer ->
-                runCatching {
-                    val metadata = ObjectMetadata().apply {
-                        contentType = filePart.headers().contentType.toString()
-                        contentLength = dataBuffer.readableByteCount().toLong()
+        return withContext(Dispatchers.IO) {
+            filePart.content()
+                .reduce { buffer1, buffer2 -> buffer1.write(buffer2) }
+                .flatMap { dataBuffer ->
+                    runCatching {
+                        resolveRequest(filePart, dataBuffer)
+                            .run { amazonS3.putObject(this) }
+                        imageUrl(filePart.filename())
+                    }.fold(
+                        onSuccess = { url ->
+                            Mono.just(url)
+                        },
+                        onFailure = { error ->
+                            Mono.error(error)
+                        }
+                    ).also {
+                        /** Release the data buffer */
+                        DataBufferUtils.release(dataBuffer)
                     }
+                }.awaitSingle()
+        }
+    }
 
-                    val inputStream = dataBuffer.asInputStream()
+    private fun resolveRequest(filePart: FilePart, dataBuffer: DataBuffer): PutObjectRequest? {
+        val metadata = resolveObjectMetadata(filePart, dataBuffer)
 
-                    val request = PutObjectRequest(imageBucketUrl(), filePart.filename(), inputStream, metadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        val inputStream = dataBuffer.asInputStream()
 
-                    amazonS3.putObject(request)
-                    imageUrl(filePart.filename())
-                }.fold(
-                    onSuccess = { url ->
-                        Mono.just(url)
-                    },
-                    onFailure = { error ->
-                        Mono.error(error)
-                    }
-                ).also {
-                    /** Release the data buffer */
-                    DataBufferUtils.release(dataBuffer)
-                }
-            }.awaitSingle()
+        return PutObjectRequest(imageBucketUrl(), filePart.filename(), inputStream, metadata)
+            .withCannedAcl(CannedAccessControlList.PublicRead)
+    }
+
+    private fun resolveObjectMetadata(
+        filePart: FilePart,
+        dataBuffer: DataBuffer
+    ): ObjectMetadata {
+        return ObjectMetadata().apply {
+            contentType = filePart.headers().contentType.toString()
+            contentLength = dataBuffer.readableByteCount().toLong()
+        }
     }
 
     fun imageBucketUrl() = "${s3Properties.bucket}/images"
