73124c12f79b1e8eec483e7ba5488419adc44b0b
DongGeon0908 <wrjssmjdhappy@gmail.com>
2024-09-24 01:18:18 +0900

    fix: 포즈 알림 조회 및 생성 진행시, 활성화 여부를 포함하여 응답을 제공한다

--- src/main/kotlin/com/hero/alignlab/domain/notification/model/response/GetPoseNotificationResponse.kt
+++ src/main/kotlin/com/hero/alignlab/domain/notification/model/response/GetPoseNotificationResponse.kt
@@ -5,12 +5,14 @@ import com.hero.alignlab.domain.notification.domain.vo.PoseNotificationDuration
 
 data class GetPoseNotificationResponse(
     val id: Long,
+    val isActive: Boolean,
     val duration: PoseNotificationDuration,
 ) {
     companion object {
         fun from(poseNotification: PoseNotification): GetPoseNotificationResponse {
             return GetPoseNotificationResponse(
                 id = poseNotification.id,
+                isActive = poseNotification.isActive,
                 duration = poseNotification.duration
             )
         }
--- src/main/kotlin/com/hero/alignlab/domain/notification/model/response/RegisterPoseNotificationResponse.kt
+++ src/main/kotlin/com/hero/alignlab/domain/notification/model/response/RegisterPoseNotificationResponse.kt
@@ -5,12 +5,14 @@ import com.hero.alignlab.domain.notification.domain.vo.PoseNotificationDuration
 
 data class RegisterPoseNotificationResponse(
     val id: Long,
+    val isActive: Boolean,
     val duration: PoseNotificationDuration,
 ) {
     companion object {
         fun from(poseNotification: PoseNotification): RegisterPoseNotificationResponse {
             return RegisterPoseNotificationResponse(
                 id = poseNotification.id,
+                isActive = poseNotification.isActive,
                 duration = poseNotification.duration
             )
         }
