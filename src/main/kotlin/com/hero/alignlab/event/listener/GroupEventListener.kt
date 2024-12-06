bdaa2666d904e9bd901003bdbebb9f38e224fc90
leeheefull <leeheefull@gmail.com>
2024-12-06 16:53:24 +0900

    fix: 그룹 생성시 그룹 유저 미생성 이슈 수정

--- src/main/kotlin/com/hero/alignlab/event/listener/GroupEventListener.kt
+++ src/main/kotlin/com/hero/alignlab/event/listener/GroupEventListener.kt
@@ -3,6 +3,7 @@ package com.hero.alignlab.event.listener
 import com.hero.alignlab.domain.group.application.GroupUserService
 import com.hero.alignlab.event.model.CreateGroupEvent
 import io.github.oshai.kotlinlogging.KotlinLogging
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
 
@@ -12,6 +13,7 @@ class GroupEventListener(
 ) {
     private val logger = KotlinLogging.logger { }
 
+    @Transactional(value = Transactional.TxType.REQUIRES_NEW)
     @TransactionalEventListener
     fun handle(event: CreateGroupEvent) {
         groupUserService.saveSync(event.group.id, event.group.ownerUid)
